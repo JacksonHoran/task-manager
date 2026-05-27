5d068232c427d3293810fe9e13ad70afd21375e8
Jackson Horan <JacksonHoran1@gmail.com>
2026-05-27 11:49:01 -0400

    add empty state

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -22,13 +22,23 @@ const removeTask = (id) => {
   <div class="">
     <TaskForm @task-added="addNewTask" />
 
-    <div class="mt-6 grid gap-3">
+    <div v-if="tasks.length > 0" class="mt-6 grid gap-3">
       <TaskItem
         v-for="task in tasks"
         :key="task.id"
         :task="task"
         @task-remove="removeTask"
       />
+    </div>
+
+    <div
+      v-else
+      class="mt-12 flex flex-col items-center justify-center text-center p-8 rounded-xl"
+    >
+      <h3 class="mt-12 text-2xl font-bold text-slate-200">All caught up!</h3>
+      <p class="text-slate-400 my-1 max-w-xs">
+        No tasks remaining. Add a new task above to get started.
+      </p>
     </div>
   </div>
 </template>
